39c5e31f01e3974cf14b62fbbd2c137cdfc33433
jonybur <jobur93@gmail.com>
2025-01-28 21:31:03 +0000

    Fix Vercel issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: './dist',
   trailingSlash: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       type: 'asset',
       generator: {
-        filename: 'static/media/[hash][ext]'
-      }
+        filename: 'static/media/[hash][ext]',
+      },
     });
     return config;
-  }
-}
+  },
+};
 
-export default nextConfig 
+export default nextConfig;
